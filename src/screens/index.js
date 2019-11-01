fe022dbc53c70696a61b57a90ad69dbf7595a425
zwonH <a.circle.one@gmail.com>
2019-11-01 19:39:48 +0900

    dzn(HomeScreen): 끼니 추가 버튼 수정
    - 끼니 추가 버튼 수정
    - kirini와 foodExample 이미지 add
    
    #1010

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,22 +1,22 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
+import {View, Text} from 'react-native';
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+
+// RCTRootView cancelTouches 오류를 해결해준다고 함...
+import 'react-native-gesture-handler';
 
 import LoginScreen from './LoginScreen';
 import HomeScreen from './HomeScreen';
 
-
-
 const AppNavigator = createStackNavigator(
-    {
-      Home: HomeScreen,
-      Login: LoginScreen,
-    },
-    {
-      initialRouteName: 'Login',
-    }
-  );
-
+  {
+    Home: HomeScreen,
+    Login: LoginScreen,
+  },
+  {
+    initialRouteName: 'Home',
+  },
+);
 
 export default createAppContainer(AppNavigator);
