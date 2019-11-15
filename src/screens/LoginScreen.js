4cd963e4ec8751cf447a1e597e0cf73c827bb8c3
Hyunsik Yang <seekeryang7@gamil.com>
2019-11-15 21:46:42 +0900

    button is adding

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,40 +1,39 @@
 import React, {Component} from 'react';
-import {
-    View,
-    Text,
-    Button,
-    StyleSheet
-} from 'react-native';
+import {View, Text, Button, StyleSheet} from 'react-native';
+import Evaluebar from '../Components/Evaluebar';
+import SelfEvaluate from './SelfEvaluate';
 
-const LoginScreen = (props) => {
-    
-    const navigationOptions = {
-        header: null,
-    };
-    return (
-        <View style={styles.container}>
-            <View style={styles.titleArea}>
-                <Text style={styles.title}>Login Screen</Text>
-                <Button
-                    title="Go to Home"
-                    onPress={() => props.navigation.navigate('Home')}
-                />
-            </View>                
-        </View>
-    );    
-}
+const LoginScreen = props => {
+  const navigationOptions = {
+    header: null,
+  };
+  return (
+    <SelfEvaluate />
+    // <View style={styles.container}>
+    //   <View style={styles.titleArea}>
+
+    //     <Text style={styles.title}>Login ddd Screen</Text>
+
+    //     <Button
+    //       title="Go to Home"
+    //       onPress={() => props.navigation.navigate('Home')}
+    //     />
+    //   </View>
+    // </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: 'white',
-        justifyContent: 'center',
-    },
-    titleArea: {
-        width: '100%',
-        
-        alignItems: 'center',
-    },
-})
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+  },
+  titleArea: {
+    width: '100%',
 
-export default LoginScreen
+    alignItems: 'center',
+  },
+});
+
+export default LoginScreen;
