20db449c5c07a4ae3618d740bda732fb6cffe858
zwonH <a.circle.one@gmail.com>
2019-11-16 16:48:21 +0900

    feat: 스플래시 추가

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
-import React from 'react';
-import AppNavigator from './src/screens';
+import React, { useEffect } from "react";
+import AppNavigator from "./src/screens";
 
-import configureStore from './src/store/configureStore';
-import {Provider} from 'react-redux';
+import configureStore from "./src/store/configureStore";
+import { Provider } from "react-redux";
+import SplashScreen from "react-native-splash-screen";
 
 const store = configureStore();
 
 const App = () => {
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     SplashScreen.hide();
+  //   }, 1000);
+  // }, []);
+
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <Provider store={store}>
       <AppNavigator />
