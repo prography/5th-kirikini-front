0306274a460f4224eb3ca155a746c4f50bca7f65
zwonH <a.circle.one@gmail.com>
2019-11-19 00:40:30 +0900

    feat(RateScreen): 라디오 버튼 구현(스타일링 안 됨)

--- src/screens/RateScreen.js
+++ src/screens/RateScreen.js
@@ -1,12 +1,86 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useState } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
-const Rate = () => {
+const mealTypes = [
+  {
+    key: 0,
+    text: '집밥'
+  },
+  {
+    key: 1,
+    text: '일밥'
+  },
+  {
+    key: 2,
+    text: '이밥'
+  },
+  {
+    key: 3,
+    text: '삼밥'
+  }
+];
+
+// const MealTypeButton = () =>
+//   mealTypes.map(item => {
+//     return (
+//       <View>
+//         <View key={item.key} style={styles.buttonContainer}>
+//           <Text>{item.text}</Text>
+//           <TouchableOpacity style={styles.circle} />
+//         </View>
+//       </View>
+//     );
+//   });
+
+const Rate = props => {
+  const [selected, setSelected] = useState(null);
+
+  const MealTypeButton = () =>
+    mealTypes.map(item => {
+      return (
+        <View>
+          <View key={item.key} style={styles.buttonContainer}>
+            <Text>{item.text}</Text>
+            <TouchableOpacity
+              style={styles.circle}
+              onPress={() => setSelected(item.key)}
+            >
+              {selected === item.key && <View style={styles.checkedCircle} />}
+            </TouchableOpacity>
+          </View>
+        </View>
+      );
+    });
   return (
-    <>
-      <Text>RateScreen by Seekeryang before 11.30</Text>
-    </>
+    <View>
+      <MealTypeButton mealTypes={mealTypes} />
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 30
+  },
+  circle: {
+    height: 20,
+    width: 20,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: '#ACACAC',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  checkedCircle: {
+    width: 14,
+    height: 14,
+    borderRadius: 7,
+    backgroundColor: '#794F9B'
+  }
+});
 
 export default Rate;
