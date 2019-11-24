179ab9e06919916a48b918c76755c5172c83c31f
길상혁 <kill5038@gmail.com>
2019-11-25 00:05:42 +0900

    kakaologin redux saga 대략 설정 서버 실섭 되면 테스트 해봐야할듯

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,6 +1,6 @@
 import {createStore, applyMiddleware, compose} from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
-import rootReducer from '../reducers';
+import rootReducer from '../store';
 import sagaMiddleware from '../sagas/middleware';
 import rootSaga from '../sagas';
 
