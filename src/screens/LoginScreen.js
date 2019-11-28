7dce99714388eac37d332784cf3af79af86121ee
zwonH <a.circle.one@gmail.com>
2019-11-29 02:24:11 +0900

    파일 정리

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,25 +1,14 @@
 import React, { Component } from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
-import Evaluebar from '../Components/Evaluebar';
-import SelfEvaluate from './SelfEvaluate';
 
 const LoginScreen = props => {
   const navigationOptions = {
     header: null
   };
   return (
-    <SelfEvaluate />
-    // <View style={styles.container}>
-    //   <View style={styles.titleArea}>
-
-    //     <Text style={styles.title}>Login ddd Screen</Text>
-
-    //     <Button
-    //       title="Go to Home"
-    //       onPress={() => props.navigation.navigate('Home')}
-    //     />
-    //   </View>
-    // </View>
+    <View>
+      <Text>hi</Text>
+    </View>
   );
 };
 
